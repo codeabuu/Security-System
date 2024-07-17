0a6fbafd3461d57bd9de7eda9565827aeb4df28b
codeabuu <abdulngui718@gmail.com>
2024-07-17 17:14:51 -0400

    updates

--- backend/camera.py
+++ backend/camera.py
@@ -3,3 +3,15 @@ import numpy as np
 import threading
 import datetime
 from storage import handle_detection
+
+class Camera:
+    net = cv.dnn.readNetFromCaffe('models/config.txt', 'models/mobilenet_iter_73000.caffemodel')
+    cap = cv.VideoCapture(0)
+    out = None
+
+    def __init__(self):
+        self.armed = False
+        self.camera_thread = None
+
+    def arm(self):
+        pass
